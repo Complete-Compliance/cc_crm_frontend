ae58a625c5ca2a6b1f26fa79fbb7e3b081a838b8
regisfaria <rgisfaria@yahoo.com.br>
2020-12-30 18:28:04 -0300

    fixed api link

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.REACT_APP_API_ENVIRONMENT === 'LOCAL'
-      ? 'http://localhost:3333/'
-      : process.env.REACT_APP_API_ADDRESS,
+  baseURL: process.env.REACT_APP_API_ADDRESS,
 });
 
 export default api;
